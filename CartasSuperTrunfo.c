05f07973db8848ebf272a35253d6fb96990ff0d1
JosephLFRangel <202501132707@alunos.estacio.br>
2025-02-11 18:36:10 -0300

    Update CartasSuperTrunfo.2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,4 +1,71 @@
 #include <stdio.h>
+int main(){
+  printf("Desafio Super Trunfo - Países \n");
+   #include <iostream>
+#include <vector>
+#include <string>
+
+class Cidade {
+public:
+    std::string codigo;
+    int populacao;
+    float area; // em km²
+    float pib; // em bilhões
+    int numPontosTuristicos;
+
+    Cidade(std::string cod, int pop, float ar, float p, int numPontos)
+        : codigo(cod), populacao(pop), area(ar), pib(p), numPontosTuristicos(numPontos) {}
+
+    void exibirDados() {
+        std::cout << "Código: " << codigo << std::endl;
+        std::cout << "População: " << populacao << std::endl;
+        std::cout << "Área: " << area << " km²" << std::endl;
+        std::cout << "PIB: " << pib << " bilhões" << std::endl;
+        std::cout << "Número de Pontos Turísticos: " << numPontosTuristicos << std::endl;
+        std::cout << std::endl;
+    }
+};
+
+void cadastrarCidades(std::vector<Cidade>& cidades) {
+    for (char estado = 'A'; estado <= 'H'; estado++) {
+        for (int i = 1; i <= 4; i++) {
+            std::string codigo = std::string(1, estado) + (i < 10 ? "0" + std::to_string(i) : std::to_string(i));
+            std::cout << "Cadastrando cidade para o código: " << codigo << std::endl;
+
+            int populacao;
+            float area, pib;
+            int numPontosTuristicos;
+
+            std::cout << "Digite a população: ";
+            std::cin >> populacao;
+
+            std::cout << "Digite a área (em km²): ";
+            std::cin >> area;
+
+            std::cout << "Digite o PIB (em bilhões): ";
+            std::cin >> pib;
+
+            std::cout << "Digite o número de pontos turísticos: ";
+            std::cin >> numPontosTuristicos;
+
+            Cidade cidade(codigo, populacao, area, pib, numPontosTuristicos);
+            cidades.push_back(cidade);
+            std::cout << "Cidade cadastrada com sucesso!\n" << std::endl;
+        }
+    }
+}
+
+int main() {
+    std::vector<Cidade> cidadesCadastradas;
+    cadastrarCidades(cidadesCadastradas);
+
+    std::cout << "Dados das cidades cadastradas:" << std::endl;
+    for (const auto& cidade : cidadesCadastradas) {
+        cidade.exibirDados();
+    }
+
+    return 0;
+}
 
 // Desafio Super Trunfo - Países
 // Tema 1 - Cadastro das Cartas
